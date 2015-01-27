62e45cae765c272013f1bb38e1616359b454493a
Juner Son <junerson@Juner-Son-ui-MacBook-Pro.local>
2015-01-28 00:23:53 +0900

    bash add

--- hadoop_util/exclude_node/lib/svc_setting.sh
+++ hadoop_util/exclude_node/lib/svc_setting.sh
@@ -2,3 +2,23 @@
 
 # file : svc_setting.sh
 # service setting.sh
+
+user_id=`id -un`
+HADOOP_PATH=''
+KAFKA_PATH=''
+GANGLIA_PATH=''
+
+if [ ${user_id} == 'hdfs' ];
+	cd ${HADOOP_PATH}/bin/ ; ./hadoop-daemon.sh start datanode; cd ${HADOOP_PATH}/logs;
+elif [ ${user_id} == 'mapred' ];
+	cd ${HADOOP_PATH}/bin/ ; ./hadoop-daemon.sh start tasktracker; cd ${HADOOP_PATH}/logs;
+elif [ ${user_id} == 'kafka' ];
+	cd ${KAFKA_PATH}
+elif [ ${user_id} == 'ganglia' ];
+	cd ${GANGLIA_PATH}; ./start-ganglia.sh
+	cat << EOF >> CRON
+@reboot ${GANGLIA_PATH}/start-ganglia.sh
+EOF
+	crontab CRON
+else 
+	echo "${user_id} has not service"
